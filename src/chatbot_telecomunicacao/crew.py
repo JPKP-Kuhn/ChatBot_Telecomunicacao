a4a6fdecb4827df9def3deb999523434528cf815
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2024-11-01 22:41:02 -0300

    Criação das ferramentas dos agentes

--- src/chatbot_telecomunicacao/crew.py
+++ src/chatbot_telecomunicacao/crew.py
@@ -1,11 +1,32 @@
+import os
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import tool, SerperDevTool, ScrapeWebsiteTool, WebsiteSearchTool, FileReadTool, PDFSearchTool
+from chatbot_telecomunicacao.tools.custom_tool import *
+from langchain_openai import ChatOpenAI
 
 # Uncomment the following line to use an example of a custom tool
 # from chatbot_telecomunicacao.tools.custom_tool import MyCustomTool
 
 # Check our tools documentations for more information on how to use them
 # from crewai_tools import SerperDevTool
+
+openai_api_key = Get_API_Key("../.env/OPENAI_API_KEY")
+serper_api_key = Get_API_Key("../.env/SERPER_API_KEY")
+pdf_search = Pdf_Search()
+os.environ["OPENAI_API_KEY"] = openai_api_key
+os.environ["SERPER_API_KEY"] = serper_api_key
+
+gpt4o_mini_llm = ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key)
+
+# Ferramenta para busca no google
+search_tool = SerperDevTool()
+
+# Ferramenta para raspagem de sites
+scrape_tool = ScrapeWebsiteTool()
+
+# Especificação de sites para raspagem
+docs_scrape_tool = WebSite_Search()
 
 @CrewBase
 class ChatbotTelecomunicacaoCrew():
@@ -16,28 +37,40 @@ class ChatbotTelecomunicacaoCrew():
 		return Agent(
 			config=self.agents_config['identificador'],
 			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
+			verbose=True,
+			tools=[search_tool, scrape_tool, docs_scrape_tool, pdf_search],
+			allow_delegation=False,
+			llm=gpt4o_mini_llm
 		)
 
 	@agent
 	def juridico(self) -> Agent:
 		return Agent(
 			config=self.agents_config['juridico'],
-			verbose=True
+			verbose=True,
+   			tools=[search_tool, scrape_tool, docs_scrape_tool, pdf_search],
+			allow_delegation=False,
+			llm=gpt4o_mini_llm
 		)
   
 	@agent
 	def tecnico(self) -> Agent:
 		return Agent(
 			config=self.agents_config['tecnico'],
-			verbose=True
+			verbose=True,
+			tools=[search_tool, scrape_tool, docs_scrape_tool, pdf_search],
+			allow_delegation=False,
+			llm=gpt4o_mini_llm
 		)
   
 	@agent
 	def supervisor(self) -> Agent:
 		return Agent(
 			config=self.agents_config['supervisor'],
-			verbose=True
+			verbose=True,
+			tools=[search_tool, scrape_tool, docs_scrape_tool, pdf_search],
+			allow_delegation=False,
+			llm=gpt4o_mini_llm
 		)
 
 	@task
--- src/chatbot_telecomunicacao/tools/custom_tool.py
+++ src/chatbot_telecomunicacao/tools/custom_tool.py
@@ -1,12 +1,48 @@
 from crewai_tools import BaseTool
+from crewai_tools import PDFSearchTool, SerperDevTool
 
-
-class MyCustomTool(BaseTool):
-    name: str = "Name of my tool"
-    description: str = (
-        "Clear description for what this tool is useful for, you agent will need this information to use it."
-    )
+class Get_API_Key(BaseTool):
+    name: str = "Get the API Key, the OpenAi or Serper"
 
     def _run(self, argument: str) -> str:
-        # Implementation goes here
-        return "this is an example of a tool output, ignore it and move along."
+        with open(argument, "r") as file:
+            return file.read()
+        return file.read().strip()
+    
+# Especificação de sites para raspagem
+class WebSite_Search(BaseTool):
+    name: str = "Search on the web"
+    description: str = (
+        "Use this to search on the web"
+    )
+
+    def _run(self):
+        search_tool = SerperDevTool(
+            website_url=[ 
+                "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/banda-larga",
+                "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/telefonia-movel",
+                "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/telefonia-fixa",
+                "https://www.gov.br/anatel/pt-br/consumidor/conheca-seus-direitos/tv-por-assinatura"
+            ]
+        )
+        return search_tool
+
+# Ferramenta para RAG em arquivos PDF. Específica para fazer buscas e extrair partes relevantes em arquivos PDF. 
+class Pdf_Search(BaseTool):
+    name: str = "Get the PDF from normas/"
+    description: str = (
+        "Use this for RAG in PDFs"
+    )
+
+    def _run(self):
+        pdf_search = PDFSearchTool(pdf="normas/335492.pdf")
+        pdf_search1 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 426, de 9 de dezembro de 2005.pdf")
+        pdf_search2 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 477, de 7 de agosto de 2007.pdf")
+        pdf_search3 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 488, de 3 de dezembro de 2007.pdf")
+        pdf_search4 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 581, de 26 de março de 2012.pdf")
+        pdf_search5 = PDFSearchTool(pdf="normas/Anatel - Resolução nº 632, de 7 de março de 2014.pdf")
+        pdf_search6 = PDFSearchTool(pdf="normas/oficio_12273325.pdf")
+        
+        return pdf_search, pdf_search1, pdf_search2, pdf_search3, pdf_search4, pdf_search5, pdf_search6
+
+
